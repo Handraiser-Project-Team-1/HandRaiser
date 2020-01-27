3a9b90cfcf5e983e486ce183f62b356c0375a646
marcialnorte <marcial.norte@boom.camp>
2020-01-27 14:59:38 +0800

    Done inital appbar component

--- g1-handraiser/src/components/class-select/ClassSelect.js
+++ g1-handraiser/src/components/class-select/ClassSelect.js
@@ -1,6 +1,19 @@
 import React from "react";
 import TopBar from "../includes/TopBar";
+import Skeleton from "@material-ui/lab/Skeleton";
+import { Grid } from "@material-ui/core";
 
 export default function ClassSelect(props) {
-  return <TopBar active={props.active}>s</TopBar>;
+  return (
+    <TopBar active={props.active}>
+      <Grid container spacing={1}>
+        <Grid item xs={6}>
+          <Skeleton variant="rect" width="100%" height="100vh" />
+        </Grid>
+        <Grid item xs={6}>
+          <Skeleton variant="rect" width="100%" height="100vh" />
+        </Grid>
+      </Grid>
+    </TopBar>
+  );
 }
