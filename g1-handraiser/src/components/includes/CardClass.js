0933c681c135c7804f37c00606ff70a5b6ccea93
marcialnorte <marcial.norte@boom.camp>
2020-01-29 14:03:57 +0800

    Revised top bar

--- g1-handraiser/src/components/includes/CardClass.js
+++ g1-handraiser/src/components/includes/CardClass.js
@@ -9,7 +9,7 @@ import Collapse from "@material-ui/core/Collapse";
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import { red,green } from "@material-ui/core/colors";
+import { red, green } from "@material-ui/core/colors";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Button from "@material-ui/core/Button";
 
@@ -34,28 +34,31 @@ const useStyles = makeStyles(theme => ({
   avatar: {
     backgroundColor: green[500],
     width: theme.spacing(7),
-    height: theme.spacing(7),
+    height: theme.spacing(7)
   },
   avatarRed: {
     backgroundColor: red[500],
     width: theme.spacing(7),
-    height: theme.spacing(7),
+    height: theme.spacing(7)
   }
 }));
 
 export default function CardClass(props) {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
-  const { title,date,description,mentor } = props
+  const { title, date, description, mentor } = props;
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
   return (
-    <Card className={classes.card}>
+    <Card className={classes.card} variant="outlined">
       <CardHeader
         avatar={
-          <Avatar aria-label="recipe" className={date ? classes.avatar : classes.avatarRed}>
+          <Avatar
+            aria-label="recipe"
+            className={date ? classes.avatar : classes.avatarRed}
+          >
             {title.charAt(0).toUpperCase()}
           </Avatar>
         }
@@ -64,11 +67,11 @@ export default function CardClass(props) {
       />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-         {description}
+          {description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <Button variant="outlined" size="small" color="primary"  >
+        <Button variant="outlined" size="small" color="primary">
           {date ? "Enter" : "Click to join the class"}
         </Button>
 
@@ -85,7 +88,7 @@ export default function CardClass(props) {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-        <Typography paragraph={false}>Mentor: {mentor}</Typography>
+          <Typography paragraph={false}>Mentor: {mentor}</Typography>
         </CardContent>
       </Collapse>
     </Card>
