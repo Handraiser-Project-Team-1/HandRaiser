0eaff020456c5add0791671d6d13ad8945c032ce
serra-vp <vincent.serra@boom.camp>
2020-02-03 11:34:12 +0800

    Added authentication key, generation of keys front and back  @admin

--- g1-handraiser/src/components/superadmin/actions/request.js
+++ g1-handraiser/src/components/superadmin/actions/request.js
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   CssBaseline,
-  Container,
+  //Container,
   Button,
   DialogTitle,
   Dialog,
   List,
   ListItem,
-  ListItemText
+  ListItemText,
+  DialogContent
 } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 import axios from "axios";
 // require('dotenv').config()
+import { Grid } from '@material-ui/core';
+import UserType from './UserType';
+
+import Notification from '../../includes/Notif';
 
 const useStyles = makeStyles(theme => ({
   bot: {
@@ -33,11 +38,17 @@ export default function Request(props) {
   const classes = useStyles();
   const { open, setOpen } = props;
   const [names, setNames] = useState([]);
-  const [disable, setDisable] = useState(false);
+  const [notif, setNotif] = useState(false);
+  const [notifDetails, setNotifDetails] = useState({
+    type: '',
+    title: '',
+    message: '',
+  })
 
-  useEffect(() => {
+  const getUserFn = () => {
     // console.log(process.env.REACT_APP_DB_URL, 'hi')
-    axios.get(`${process.env.REACT_APP_DB_URL}/api/users`).then(res => {
+    // axios.get(`${process.env.REACT_APP_DB_URL}/api/users`).then(res => {
+    axios.get(`http://localhost:3001/api/users`).then(res => {
       var temp = [];
       res.data.map(x => {
         temp.push({
@@ -51,67 +62,68 @@ export default function Request(props) {
       });
       setNames(temp);
     });
+  }
+
+  useEffect(() => {
+    getUserFn();
   }, []);
+
   const closeAdd = () => {
     setOpen(false);
   };
-  const choose = e => {
-    axios
-      .post(`${process.env.REACT_APP_DB_URL}/api/user/${e.target.id}`, {
-        userd_id: e.target.id,
-        user_type: e.target.value
-      })
-      .then(res => {
-        alert("Successfully assigned");
-        setDisable(true);
-      });
-  };
+
+  const setNotifDetailsFn = (type,title,message) => {
+    setNotifDetails({
+      type: type,
+      title: title,
+      message: message,
+    })
+  }
+
+  const openNofif = () => {
+    setNotif(true);
+  }
+  
   return (
     <Dialog aria-labelledby="simple-dialog-title" open={open}>
-      <Container component="main">
-        <CssBaseline />
-        <DialogTitle id="simple-dialog-title">
-          Login Requests
-          <Button color="primary" onClick={closeAdd}>
-            <CloseIcon />
-          </Button>
-        </DialogTitle>
-        {names.map((x, id) => {
-          return (
-            <List key={id}>
-              <ListItem>
-                <img alt={x.image} src={x.image} className={classes.pic} />
-                <ListItemText>
-                  {x.lname}, {x.fname}
-                </ListItemText>
-                <button
-                  onClick={choose}
-                  value={"student"}
-                  id={x.uid}
-                  className={classes.bot}
-                  disabled={disable}
-                >
-                  Student
-                </button>
-                <button
-                  onClick={choose}
-                  value={"mentor"}
-                  id={x.uid}
-                  className={classes.bot}
-                  disabled={disable}
-                >
-                  Mentor
-                </button>
-                <Button>Send Key</Button>
-              </ListItem>
-            </List>
-          );
-        })}
-        <Button type="submit" fullWidth variant="contained" color="primary">
-          {" "}
-          Add
+      <Notification
+        type={notifDetails.type}
+        title={notifDetails.title}
+        message={notifDetails.message}
+        open={notif}
+        setOpen={setNotif}
+      />
+      {/* <Container component="main"> */}
+      <CssBaseline />
+      <DialogTitle id="simple-dialog-title">
+        Login Requests
+        <Button color="primary" onClick={closeAdd}>
+          <CloseIcon />
         </Button>
-      </Container>
+      </DialogTitle>
+      <DialogContent dividers>
+        <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="stretch"
+        >
+          <List >
+              {names.map((x, id)=>{
+                  return(
+                      <ListItem key={id}>
+                          <img alt={x.image} src={x.image} className={classes.pic}/>
+                          <ListItemText>
+                              {x.lname}, {x.fname}
+                          </ListItemText>
+                          <UserType setNotifDetailsFn={setNotifDetailsFn} openNofif={openNofif} getUserFn={getUserFn} userid={x.uid}/>
+                      </ListItem>
+                  )
+              })}
+          </List>
+        </Grid> 
+      </DialogContent>
+      {/* </Container> */}
     </Dialog>
   );
 }
