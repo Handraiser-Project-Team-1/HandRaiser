d6b0c11936b22cc40b648c93869d5730dec922b6
serra-vp <vincent.serra@boom.camp>
2020-02-10 16:32:22 +0800

    added some changes in sending email at admin

--- g1-handraiser/src/components/superadmin/Request.js
+++ g1-handraiser/src/components/superadmin/Request.js
@@ -4,17 +4,33 @@ import { Grid, Paper, Divider } from "@material-ui/core";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
-import CustomizedSelects from "./includes/FormSelect";
-import Button from "@material-ui/core/Button";
+import RequestButton from "./includes/RequestButton";
+import Notification from "./includes/Notif";
 
 export default function Request() {
   const [list, setList] = useState([]);
   const [pending, setPending] = useState([]);
-  const [type, setType] = useState("");
+  const [notif, setNotif] = useState(false);
+  const [notifDetails, setNotifDetails] = useState({
+    type: '',
+    title: '',
+    message: '',
+  })
+
+  const setNotifDetailsFn = (type,title,message) => {
+    setNotifDetails({
+      type: type,
+      title: title,
+      message: message,
+    })
+  }
+
+  const openNofif = () => {
+    setNotif(true);
+  }
 
   useEffect(() => {
     getUserFn();
@@ -58,28 +74,15 @@ export default function Request() {
     })
   }
 
-  const submit = val => {
-    Axios.post(`${process.env.REACT_APP_DB_URL}/api/key`, {
-      id: val,
-      type: type
-    })
-    .then(res => {
-      getUserFn();
-      pendingList();
-    })
-    .catch(error => {
-      //console.error(err.response.data.error)
-      let err = String(error).match(/\w+$/g).join();
-      if(err === '400'){
-        permissionFn();
-        return;
-      }
-      console.error(error);
-    });
-  };
-
   return (
     <Grid container spacing={1}>
+      <Notification
+        type={notifDetails.type}
+        title={notifDetails.title}
+        message={notifDetails.message}
+        open={notif}
+        setOpen={setNotif}
+      />
       <Grid item xs={6}>
         <Paper elevation={0}>
           <Typography variant="h6" style={{ padding: 10, paddingLeft: 20 }}>
@@ -96,34 +99,7 @@ export default function Request() {
                     primary={`${val.user_fname} ${val.user_lname}`}
                     secondary={val.user_email}
                   />
-                  <ListItemSecondaryAction>
-                    <Grid
-                      container
-                      direction="row"
-                      justify="center"
-                      alignItems="center"
-                    >
-                      <Grid item>
-                        <CustomizedSelects
-                          stat={val.user_type}
-                          setType={setType}
-                        />
-                      </Grid>
-                      <Grid item>
-                        <Button
-                          size="small"
-                          variant="contained"
-                          color="primary"
-                          onClick={() => submit(val.userd_id)}
-                          disabled={
-                            type === "pending" || type === "" ? true : false
-                          }
-                        >
-                          Send Key
-                        </Button>
-                      </Grid>
-                    </Grid>
-                  </ListItemSecondaryAction>
+                  <RequestButton key={val.userd_id} val={val} setNotifDetailsFn={setNotifDetailsFn} openNofif={openNofif} permissionFn={permissionFn} getUserFn={getUserFn} pendingList={pendingList}/>
                 </ListItem>
                 <Divider />
               </React.Fragment>
--- migrations/1581300829291_add-student-table.js
+++ migrations/1581300829291_add-student-table.js
@@ -16,11 +16,7 @@ exports.up = (pgm) => {
         },
         status: {
             type: "boolean",
-<<<<<<< HEAD:migrations/1581298979055_add-student-table.js
-            notNull: true
-=======
             notNull: true,
->>>>>>> g1-develop:migrations/1581300829291_add-student-table.js
         }
     })
 };
