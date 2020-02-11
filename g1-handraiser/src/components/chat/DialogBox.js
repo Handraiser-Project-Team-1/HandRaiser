c57ad71e6494373878a440e512c9d1f8ebc7d3d6
Mark Jowen Medes <markjowena.medes@gmail.com>
2020-02-11 16:39:31 +0800

    fix chat(socket request)

--- g1-handraiser/src/components/chat/DialogBox.js
+++ g1-handraiser/src/components/chat/DialogBox.js
@@ -15,7 +15,7 @@ let user = 'mark' + Math.floor(Math.random() * Math.floor(20000));
 export default function DialogBox({ handleClose, open }) {
   const [state, setState] = useState(1);
   const [name] = useState(user)
-  const [room] = useState('Room 101')
+  const [room] = useState('')
   const [messages, setMessages] = useState([])
   const [message, setmessage] = useState('')
   const [feedback, setFeedbAck] = useState('')
@@ -30,12 +30,34 @@ export default function DialogBox({ handleClose, open }) {
     }
   };
 
+  // useEffect(() => {
+  //   socket = io(ENDPOINT)
+  //   console.log(name);
+  //   console.log(socket);
+  //   socket.emit('join', { name, room });
+  // }, [ENDPOINT, name, room])
+
   useEffect(() => {
     socket = io(ENDPOINT)
-    console.log(name);
     console.log(socket);
-    socket.emit('join', { name, room });
-  }, [ENDPOINT, name, room])
+    let session_id;
+
+    let data = sessionStorage.getItem('sessionId');
+    if (data == null) {
+      session_id = null
+      socket.emit('join', { name, sessionId: session_id })
+    } else {
+      console.log('show prev '+data)
+      session_id = data
+      socket.emit('join', { name, sessionId: session_id })
+    }
+  }, [ENDPOINT, name])
+
+  useEffect(() => {
+    socket.on("set-session-acknowledgement", function (data) {
+      sessionStorage.setItem('sessionId', data.sessionId);
+    })
+  })
 
   useEffect(() => {
     socket.on('typing', (data) => {
@@ -57,13 +79,13 @@ export default function DialogBox({ handleClose, open }) {
   }, [messages]);
 
   const keypress = (data) => {
-    socket.emit('typing', data+' is typing...');
+    socket.emit('typing', data + ' is typing...');
   }
   useEffect(() => {
     const msg = message;
-    if(msg.length > 0){
-      
-    }else{
+    if (msg.length > 0) {
+
+    } else {
       typing()
     }
   })
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const secret = process.env.REACT_APP_SECRET_KEY;
 const user = require("./controllers/user/user");
 const admin = require("./controllers/admin/admin");
-const { addUser, removeUser, getUser } = require("./users");
+const { addUser, removeUser, getUser, updateUser } = require("./users");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const email = require("./controllers/email/email");
@@ -24,20 +24,30 @@ massive({
   io.on("connection", socket => {
     console.log("we have a new connection!");
 
-    socket.on("join", ({ name, room }, callback) => {
-      console.log(name, room);
-      const { error, user } = addUser({ id: socket.id, name, room });
-
-      if (error) return callback(error);
-
-      socket.emit("message", {
-        user: "admin",
-        text: `${user.name}, welcome to the room`
-      });
-      socket.broadcast
-        .to(user.room)
-        .emit("message", { user: "admin", text: `${user.name}, has joined!` });
-      socket.join(user.room);
+    socket.on('join', function ({ name, sessionId }) {
+      if (sessionId == null) {
+        var session_id = '101';
+        const { user } = addUser({ id: socket.id, name, room: session_id });
+        socket.join(user.room, function (res) {
+          socket.emit("message", {
+            user: "admin",
+            text: `${user.name}, welcome to the room`
+          });
+          console.log("joined successfully ")
+          socket.emit("set-session-acknowledgement", { sessionId: session_id })
+        })
+      } else {
+        socket.room = sessionId;
+        const { user } = addUser({ id: socket.id, name, room: socket.room });
+        socket.join(user.room, function (res) {
+          socket.emit("message", {
+            user: "admin",
+            text: `${name}, welcome to the room`
+          });
+          console.log("joined successfully ")
+          socket.emit("set-session-acknowledgement", { sessionId: sessionId })
+        })
+      }
     });
 
     socket.on("sendMessage", (message, callback) => {
@@ -75,7 +85,7 @@ massive({
   app.post("/api/users/", user.createUsers);
   app.get("/api/users", user.getUsers);
   app.post("/api/user", user.getUser);
-  app.get("/api/protected/data", function(req, res) {
+  app.get("/api/protected/data", function (req, res) {
     if (!req.headers.authorization) {
       return res.status(401).end();
     }
