b2e81634ab84e54baf658b8dd69fbc6f3710cd90
serra-vp <vincent.serra@boom.camp>
2020-02-12 11:25:50 +0800

    merged add-student-table conflict

--- migrations/1581300829291_add-student-table.js
+++ migrations/1581300829291_add-student-table.js
@@ -2,25 +2,6 @@
 
 exports.shorthands = undefined;
 
-<<<<<<< HEAD
-exports.up = pgm => {
-  pgm.createTable("student", {
-    student_id: {
-      type: "serial",
-      primaryKey: true
-    },
-    user_id: {
-      type: "integer",
-      references: "user_type",
-      notNull: true,
-      onDelete: "cascade"
-    },
-    status: {
-      type: "boolean",
-      notNull: true
-    }
-  });
-=======
 exports.up = (pgm) => {
     pgm.createTable("student",{
         student_id: {
@@ -38,7 +19,6 @@ exports.up = (pgm) => {
             notNull: true,
         }
     })
->>>>>>> 4c5afd22a7de152488306c9367a569c63a200749
 };
 
 exports.down = pgm => {};
