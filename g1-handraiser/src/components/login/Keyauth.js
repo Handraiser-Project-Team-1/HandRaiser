0d979d647861cd041a878dabd37a11babfe96781
franco <ifurungfranco@gmail.com>
2020-02-21 17:29:04 +0800

    fix route auth and server

--- g1-handraiser/src/components/login/Keyauth.js
+++ g1-handraiser/src/components/login/Keyauth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "@material-ui/core/Card";
 import { makeStyles } from "@material-ui/core/styles";
 import { TextField, Button, Grid, Typography } from "@material-ui/core";
@@ -71,6 +71,24 @@ function Keyauth() {
     message: ""
   });
 
+  useEffect(()=>{
+    if (localStorage.getItem("tokenid")) {
+      axios({
+        method: "get",
+        url: `${process.env.REACT_APP_DB_URL}/api/type/${localStorage.getItem(
+          "uid"
+        )}`
+      }).then(res => {
+        if(res.data[0].user_type === "pending"){
+          history.push("/authentication")
+        }else if(res.data[0].user_type === "student"){
+          history.push("/classes")
+        }else if(res.data[0].user_type === "mentor"){
+          history.push("myclasslist")
+        }
+      })}
+  },[])
+
   const setNotifDetailsFn = (type, title, message) => {
     setNotifDetails({
       type: type,
@@ -100,6 +118,7 @@ function Keyauth() {
     })
       .then(response => {
         localStorage.setItem("id", JSON.stringify(response.data.id));
+        localStorage.setItem("uid", JSON.stringify(response.data.user_id));
         if (tokenObj !== null) {
           setTimeout(() => {
             if (response.data.type === "student") {
